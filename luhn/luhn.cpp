00bfa9167a420e4ed16c6f72aa33d125f72cb5bb
Eloide Novela <eloide.novela@outlook.com>
2024-09-12 00:22:53 +0200

    feat: added luhn algorithm

--- luhn/luhn.cpp
+++ luhn/luhn.cpp
@@ -3,7 +3,26 @@
 namespace luhn {
 
     bool checker(long long card_number, int & digits) {
-        
-    }
+        short int right_digits = 0, left_digits = 0, check_sum = 0;
 
+        while (card_number != 0) {
+            digits++;
+
+            if (digits % 2 != 0) {
+                left_digits += card_number % 10;
+            } else {
+                if ((2 * (card_number % 10)) > 9) {
+                    right_digits += 2 * (card_number % 10) - 9;
+                } else {
+                    right_digits += 2 * (card_number % 10);
+                }
+            }
+
+            card_number = card_number / 10;
+        }
+
+        check_sum = left_digits + right_digits;
+
+        return (check_sum % 10 == 0);
+    }
 }
--- util/util.cpp
+++ util/util.cpp
@@ -3,6 +3,23 @@
 namespace card {
 
     std::string type_is(long long card_number, int digits) {
+        long long int start_digits = 0;
 
+        for (int i = 1 ; i <= digits - 2 ; i++) {
+            start_digits = card_number / 10;
+            card_number = start_digits;		
+        }
+
+        if (((digits == 16) || (digits == 13)) && (start_digits >= 40 && start_digits <=49)) {
+            return "VISA";
+
+        } else if ((digits == 16) && (start_digits >= 51 && start_digits <= 55)) {
+            return "MASTERCARD";
+
+        } else if ((digits == 15) && (start_digits == 34 || start_digits == 37)) {
+            return "AMEX";
+        }
+
+        return "unknown card";
     }
 }
